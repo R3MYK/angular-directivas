a79bbdc0ee1bd1c4413903fec3e240078af6a714
Nicolas Martínez <remyblunt_@hotmail.com>
2019-04-11 14:31:05 -0400

    Commir NgFor y NgIf

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -6,11 +6,74 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./body.component.css']
 })
 export class BodyComponent implements OnInit {
-  categorias: any = [{ "codigo": 1, "texto": "Politica" },
-  { "codigo": 2, "texto": "Educación" }, { "codigo": 3, "texto": "Seguridad"}]
-  constructor() { }
+  categorias: any = [
+    { "codigo": 0, "texto": "Todos" },
+    { "codigo": 1, "texto": "Politica" },
+    { "codigo": 2, "texto": "Educación" },
+    { "codigo": 3, "texto": "Seguridad" },
+  ];
+
+  temporal: any;
+
+  noticias: any = [{
+    "categoria": {
+      "codigo": 1,
+      "texto": "Politica"
+    },
+    "titulo": "Prueba de Titulo",
+    "text": "Lorem ipsum dolor sit amet consectetur,adipisicing elit. Ut, sit deleniti Consectetur neque perferendis eum odio possimus harum perspiciatis architecto",
+    "imagen": "http://www.trabajadores.cu/wp-content/uploads/2015/04/entrevistaperfecta-informatica31.jpg"
+  },
+  {
+    "categoria": {
+      "codigo": 2,
+      "texto": "Politica"
+    },
+    "titulo": "Prueba de Titulo",
+    "text": "Lorem ipsum dolor sit amet consectetur,adipisicing elit. Ut, sit deleniti Consectetur neque perferendis eum odio possimus harum perspiciatis architecto",
+    "imagen": "http://www.trabajadores.cu/wp-content/uploads/2015/04/entrevistaperfecta-informatica31.jpg"
+  }, {
+    "categoria": {
+      "codigo": 1,
+      "texto": "Politica"
+    },
+    "titulo": "Prueba de Titulo",
+    "text": "Lorem ipsum dolor sit amet consectetur,adipisicing elit. Ut, sit deleniti Consectetur neque perferendis eum odio possimus harum perspiciatis architecto",
+    "imagen": "http://www.trabajadores.cu/wp-content/uploads/2015/04/entrevistaperfecta-informatica31.jpg"
+  }, {
+    "categoria": {
+      "codigo": 1,
+      "texto": "Politica"
+    },
+    "titulo": "Prueba de Titulo",
+    "text": "Lorem ipsum dolor sit amet consectetur,adipisicing elit. Ut, sit deleniti Consectetur neque perferendis eum odio possimus harum perspiciatis architecto",
+    "imagen": "http://www.trabajadores.cu/wp-content/uploads/2015/04/entrevistaperfecta-informatica31.jpg"
+  }, {
+    "categoria": {
+      "codigo": 2,
+      "texto": "Politica"
+    },
+    "titulo": "Prueba de Titulo",
+    "text": "Lorem ipsum dolor sit amet consectetur,adipisicing elit. Ut, sit deleniti Consectetur neque perferendis eum odio possimus harum perspiciatis architecto",
+    "imagen": "http://www.trabajadores.cu/wp-content/uploads/2015/04/entrevistaperfecta-informatica31.jpg"
+  }];
+  constructor() {
+    //Temporal es igual a  las noticias
+    this.temporal = this.noticias;
+  }
 
   ngOnInit() {
+  }
+
+  eventocontrol(evento) {
+    if (evento.target.value == 0) {
+      this.noticias = this.temporal;
+    } else {
+      this.noticias = this.noticias.filter((item) => {
+        return item.categoria.codigo == evento.target.value;
+        //Filtra y compara categoria codigo es igual a evento del valor 
+      });
+    }
   }
 
 }
